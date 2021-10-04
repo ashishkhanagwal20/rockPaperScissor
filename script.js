6f19cbdef19d39a206f76115f8da788404dc34bd
Ashish Kumar <ashishstudy20@gmail.com>
2021-10-04 09:36:03 +0530

    Complete game logic

--- script.js
+++ script.js
@@ -9,4 +9,35 @@ if (playerChoiceStr == "ROCK") {
 } else if (playerChoiceStr == "SCISSOR") {
   playerChoiceNum = 2;
 }
-console.log(playerChoiceNum);
+
+let computerChoice = Math.floor(Math.random() * 3);
+
+let gameFunction = (playerSelection, computerSelection) => {
+  if (playerSelection === 0) {
+    if (computerSelection === 0) {
+      alert("It's a Tie, You both choose Rock");
+    } else if (computerSelection === 1) {
+      alert("You lose, computer choose paper");
+    } else if (computerSelection === 2) {
+      alert("You win, computer choose scissor");
+    }
+  } else if (playerSelection === 1) {
+    if (computerSelection === 0) {
+      alert("You Win, Computer choose Rock");
+    } else if (computerSelection === 1) {
+      alert("it's a tie, you both choose paper");
+    } else if (computerSelection === 2) {
+      alert("You lose, computer choose scissor");
+    }
+  } else if (playerSelection === 2) {
+    if (computerSelection === 0) {
+      alert("You lose, Computer choose Rock");
+    } else if (computerSelection === 1) {
+      alert("You win, computer choose paper");
+    } else if (computerSelection === 2) {
+      alert("It's a tie, you both choose scissor");
+    }
+  }
+};
+
+gameFunction(playerChoiceNum, computerChoice);
